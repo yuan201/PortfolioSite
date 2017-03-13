088907be69ef78b8c448fdcd4641ddab22f11f5d
Yuan Yuan <yyuan201@163.com>
2017-03-13 21:41:28 +0800

    fix custom filter bug

--- benchmarks/templatetags/bench_extras.py
+++ benchmarks/templatetags/bench_extras.py
@@ -5,9 +5,15 @@ register = template.Library()
 
 @register.filter(name='percent')
 def percent(value):
-    return "{:.1f}%".format(value*100)
+    if isinstance(value, float):
+        return "{:.1f}%".format(value*100)
+    else:
+        return ""
 
 
 @register.filter(name='change')
 def change(value):
-    return "{:.2f}%".format((value-1)*100)
+    if isinstance(value, float):
+        return "{:.2f}%".format((value-1)*100)
+    else:
+        return ""
