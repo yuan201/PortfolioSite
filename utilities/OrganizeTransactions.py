a1b35c2fab387fa8c9494ca37b50c429922e297d
Yuan Yuan <yyuan201@gmail.com>
2017-07-11 15:16:17 +0800

    add utilities

---
+++ utilities/OrganizeTransactions.py
@@ -0,0 +1,95 @@
+import sys
+import csv
+from collections import namedtuple
+import datetime as dt
+
+
+Header = ['symbol','name','type_','date','shares','price',
+          'cashvalue', 'fee', 'dividend', 'ratio','currency']
+#Transaction = namedtuple('Transaction', Header)
+
+
+def organize_zszq(file):
+    lines = []
+    with open(file) as csvfile:
+        csvreader = csv.DictReader(csvfile, )
+        for line in csvreader:
+            print(line)
+            l = organize_line_zszq(line)
+            if l['type_'] != 'invalid':
+                lines.append(l)
+    return lines
+
+
+def organize_line_zszq(line):
+    t = dict(
+        symbol = line['证券代码'],
+        name =  line['证券名称'],
+        date = zszq_datetime(line['成交日期'], line['成交时间']),
+        type_ = zszq_type(line['买卖标志']),
+        shares = 0,
+        price = 0,
+        cashvalue = 0,
+        fee = 0,
+        dividend = 0,
+        ratio = 0,
+        currency='cny',
+    )
+
+    if t['type_'] == 'buy':
+        t['shares'] = int(line['成交数量'])
+        t['price'] = float(line['成交价格'])
+        t['cashvalue'] = -float(line['成交金额'])
+    elif t['type_'] == 'sell':
+        t['shares'] = int(line['成交数量'])
+        t['price'] = float(line['成交价格'])
+        t['cashvalue'] = float(line['成交金额'])
+    elif t['type_'] == 'dividend':
+        t['dividend'] = float(line['成交金额'])
+
+    if t['symbol'].startswith('60'):
+        t['symbol'] += '.SS'
+    elif t['symbol'].startswith('00'):
+        t['symbol'] += '.SZ'
+        
+    return t
+
+
+def zszq_type(type):
+    if type == '股息入帐':
+        return 'dividend'
+    elif type == '买入':
+        return 'buy'
+    elif type == '卖出':
+        return 'sell'
+    else:
+        return 'invalid'   
+
+    
+def zszq_datetime(date_, time_):
+    year = int(date_[:4])
+    month = int(date_[4:6])
+    day = int(date_[6:])
+    time_ = time_.split(':')
+    hour = int(time_[0])
+    minute = int(time_[1])
+    second = int(time_[2])
+    return dt.datetime(year, month, day, hour, minute, second)
+    
+    
+def dumplines(lines, file):
+    print(lines)
+
+
+def usage():
+    print("Usage:\n  {} type infile outfile".format(sys.argv[0]))
+    exit()
+            
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        usage()
+
+    if sys.argv[1] == "zszq":
+        lines = organize_zszq(sys.argv[2])
+        dumplines(lines, sys.argv[3])
+        
---
+++ utilities/quote2excel.py
@@ -0,0 +1,79 @@
+###############################################
+# use tushare to get quotes
+# and save to csv for excel
+# only workon for stocks listed
+# in Shanghai and Shenzheng Exchange
+###############################################
+
+import tushare as ts
+import pandas as pd
+import csv
+from collections import namedtuple
+import datetime as dt
+import sys
+
+Security = namedtuple('Security',
+                      ['symbol','exchange','name','index','longname'])
+
+# load symbols from csv file
+def read_symbol_file(file):
+    securities = {}
+    with open(file) as csvfile:
+        sym_reader = csv.reader(csvfile)
+        for row in sym_reader:
+            se = row[0].split('.')
+            securities[row[0]] = Security(se[0],se[1],row[1],
+                                          len(se)==3,row[0])
+    return securities
+
+
+# get quotes from tushare and put into a DataFrame
+def get_quotes(securities, daterange):
+    quotes = pd.DataFrame(index=daterange)
+    for sec in securities.values():
+        if sec.exchange == 'SS' or sec.exchange == 'SZ':
+            quote = ts.get_k_data(code=sec.symbol,
+                                  autype=None, index=sec.index);
+            quote = convert_date(quote)
+            quotes[sec.longname] = quote['close']
+        elif sec.exchange == 'HK':
+            pass # get quotes from yahoo
+    return quotes
+
+
+# date from tushare is just orinary string, need to convert
+def convert_date(quote):
+    quote['date'] = pd.to_datetime(quote['date'])
+    quote.set_index('date', inplace=True)
+    return quote
+    
+
+# save quotes to csv file
+def save_quotes(quotes,file):
+    pass
+
+
+def usage():
+    print("usage:\n{} symbols.csv startdate enddate quotes.csv".format(
+        sys.argv[0]))
+
+def addname(csvlist, secs):
+    names = [secs[i].name for i in csvlist[0].split(',')[1:]]
+    namestr = ',' + ','.join(names)
+    csvlist.insert(1, namestr)
+    return csvlist
+
+def save2csv(csvlist, csvfile):
+    with open(csvfile, 'w') as file:
+        file.write('\n'.join(csvlist))
+
+if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        usage()
+        exit()
+    secs = read_symbol_file(sys.argv[1])
+    daterange = pd.bdate_range(sys.argv[2], sys.argv[3])
+    quotes = get_quotes(secs, daterange)
+    csvlist = quotes.to_csv().split()
+    csvlist = addname(csvlist, secs)
+    save2csv(csvlist, sys.argv[4])
